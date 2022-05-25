b163a47dc804c47af9bd9961728c7437b5bb9e06
Gary Williams <w_gwilliams@yahoo.com>
2022-05-25 18:33:18 -0500

    create candidate

--- server.js
+++ server.js
@@ -29,9 +29,37 @@ console.log('Connect to the election database')
 //     });
 //   });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
+
+// db.query('SELECT * FROM candidates WHERE id = 1', (err, row) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+//The DELETE statement has a question mark (?) that denotes a placeholder, making this a prepared statement. A prepared statement can execute the same SQL statements repeatedly using different values in place of the placeholder.
+// db.query('DELETE FROM candidates WHERE id=?', 1, (err,result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params,(err, result) => {
+    if (err){
+        console.log(err);
+    }
+    console.log(result)
+})
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
