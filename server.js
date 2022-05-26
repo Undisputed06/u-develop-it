a5dd47d4e57f18035346a675162dddbdbc22d262
Gary Williams <w_gwilliams@yahoo.com>
2022-05-25 20:23:01 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,6 +23,22 @@ const db = mysql.createConnection(
 console.log('Connect to the election database')
 )
 
+//Get all candidates
+
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) =>{
+        if(err){
+            res.status(500).json({ error: err.message})
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+    })
+})
 
 // app.get('/', (req, res) => {
 //     res.json({
@@ -33,32 +50,94 @@ console.log('Connect to the election database')
 //     console.log(rows);
 //   });
 
-// db.query('SELECT * FROM candidates WHERE id = 1', (err, row) => {
-//     if(err){
-//         console.log(err);
-//     }
-//     console.log(row);
-// })
+app.get('/api/candidate/:id', (req, res)=> {
+    const sql = `SELECT * FROM candidates WHERE id = ?`
+    const params = [req.params.id];
+
+    db.query(sql, params,(err, row) => {
+        if(err){
+            res.status(400).json({error: err.message})
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+
+
+})
+
 
 //The DELETE statement has a question mark (?) that denotes a placeholder, making this a prepared statement. A prepared statement can execute the same SQL statements repeatedly using different values in place of the placeholder.
-// db.query('DELETE FROM candidates WHERE id=?', 1, (err,result) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
+app.delete('/api/candidate/:id', (req, res) =>{
+    const sql = 'DELETE FROM candidates WHERE id = ?';
+    const params = [req.params.id]
+
+    db.query(sql, params, (err,result) => {
+        if(err) {
+            res.status(400).json({error: res.message}); 
+        } else if (!result.affectedRows){
+            res.json({
+                message: 'Candidate not found'
+            });
+        }else{
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            })
+        }
+        
+        })
+    });
+
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(
+      body,
+      'first_name',
+      'last_name',
+      'industry_connected'
+    );
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+      VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+  
+
+
+
+
 
 //Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-                VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params,(err, result) => {
-    if (err){
-        console.log(err);
-    }
-    console.log(result)
-})
+// db.query(sql, params,(err, result) => {
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log(result)
+// })
 
 
 // Default response for any other request (Not Found)
